efe647377fc17b8c2132e61e87c9cf8ea9d33cf2
hyunlee1999 <hyunlee1999@gmail.com>
2021-02-08 19:35:04 -0600

    Create Database, Create Dummy Model

--- calendarSite/calendarSite/settings.py
+++ calendarSite/calendarSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "calendarApp.apps.CalendarappConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
