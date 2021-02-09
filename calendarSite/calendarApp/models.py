b3b414913aca9deaa94b2a365bbaf7b285dccf96
hyunlee1999 <hyunlee1999@gmail.com>
2021-02-09 11:26:56 -0600

    Create Group, TodoList, and TodoItem Models

--- calendarSite/calendarApp/models.py
+++ calendarSite/calendarApp/models.py
@@ -1,4 +1,23 @@
 from django.db import models
 
-class DummyModel(models.Model):
-    dummyText = models.CharField(max_length=200)
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class TodoList(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class TodoItem(models.Model):
+    todoList = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    deadline = models.DateField()
+
+    def __str__(self):
+        return self.name
