0f2780335ee07ab07e253bf3bd5ee8f92cc9d9e3
hyunlee1999 <hyunlee1999@gmail.com>
2021-02-16 17:10:04 -0600

    Worked on Index View and Templates

--- calendarSite/calendarApp/views.py
+++ calendarSite/calendarApp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Group, TodoList, TodoItem
 
 def index(request):
-    return HttpResponse("This is the homepage of CalendarApp")
+    num_Groups = Group.objects.all().count()
+    num_TodoList = TodoList.objects.all().count()
+    num_TodoItem = TodoItem.objects.all().count()
+
+    context = {
+        "num_Groups": num_Groups,
+        "num_TodoList": num_TodoList,
+        "num_TodoItem": num_TodoItem,
+    }
+
+    return render(request, "index.html", context=context)
