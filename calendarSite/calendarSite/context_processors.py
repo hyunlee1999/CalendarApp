38fe2b615f11aed39329a2b338cd0e07ead304c2
hyunlee1999 <hyunlee1999@gmail.com>
2021-04-07 12:55:59 -0500

    assign everything to Demo

--- calendarSite/calendarSite/context_processors.py
+++ calendarSite/calendarSite/context_processors.py
@@ -1,11 +1,13 @@
 from calendarApp.models import Group, TodoList, TodoItem
+from django.contrib.auth.models import User
+
 
 def count(request):
     if request.user.is_authenticated:
         return {
             'num_Group': Group.objects.filter(user=request.user).count(),
-            'num_TodoList': TodoList.objects.all().count(),
-            'num_TodoItem': TodoItem.objects.filter(completed=False).count(),
+            'num_TodoList': TodoList.objects.filter(user=request.user).count(),
+            'num_TodoItem': TodoItem.objects.filter(user=request.user).count(),
         }
     else:
         return {
