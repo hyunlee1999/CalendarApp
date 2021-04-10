366fcab42e33a9850977f8bc27624f1a7bf4fa23
hyunlee1999 <hyunlee1999@gmail.com>
2021-04-10 17:01:43 -0500

    deploy

--- calendarSite/calendarSite/settings.py
+++ calendarSite/calendarSite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +130,5 @@ LOGIN_URL = "login"
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+django_heroku.settings(locals())
